c6f0907363ff82083ead64f267784b81e6276b3d
rolandtothu <contact@rolandtoth.hu>
2024-12-31 19:34:46 +0100

    chore: try using event meta with zod (wip)

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,16 +1,21 @@
 import {defineCollection, z} from 'astro:content';
-
 import {glob} from 'astro/loaders';
+
+const INTERNAL_LINK_REGEX = new RegExp(/^\/(?!\/)[^?\n]+(.)*$/);
+const EventCategory = z.enum(["uveghuta-kupa"]);
+const EventMetaKey = z.enum(["Limitált indulási létszám"]);
 
 const events = defineCollection({
   loader: glob({pattern: "**/*.md", base: "./src/data/events"}),
   schema: z.object({
-    type: z.enum(['uveghuta-kupa']),
+    type: EventCategory,
     title: z.string(),
     slug: z.string(),
     excerpt: z.string(),
     year: z.number(),
     pubDate: z.date(),
+    eventMeta: z.object({key: EventMetaKey, value: z.string().or(z.number())}).array().optional(),
+    links: z.object({name: z.string(), url: z.string().url().or(z.string().regex(INTERNAL_LINK_REGEX))}).array().optional(),
   }),
 });
 
--- src/data/events/uveghuta-kupa/uveghuta-kupa-2023.md
+++ src/data/events/uveghuta-kupa/uveghuta-kupa-2023.md
@@ -5,6 +5,14 @@ slug: uveghuta-kupa-2023
 excerpt: XX. Üveghuta Kupa excerpt goes here
 year: 2023
 pubDate: 2023-10-06
+eventMeta:
+  - key: Limitált indulási létszám
+    value: 220
+links:
+  - name: Versenykiírás
+    url: /rendezvenyek/xxi-uveghuta-kupa/
+  - name: External link
+    url: https://store.pwc.de
 ---
 
 2024 - Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
