b83f8ab0a1e6ce8a99f20fc4831ac9bfa09b2375
rolandtothu <contact@rolandtoth.hu>
2025-01-01 20:34:33 +0100

    feat: fonts, css updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-import mdx from '@astrojs/mdx';
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [mdx()],
 });
