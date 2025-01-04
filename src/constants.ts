31a11d3f6f518272bcf90efda3663999743a2da4
rolandtothu <contact@rolandtoth.hu>
2025-01-04 12:08:28 +0100

    feat: static pages (wip), fonts refactor, etc

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const siteName = 'Kis-Galya KSE';
 export const siteNameFull = 'Kis-Galya Közhasznú Sport Egyesület';
 export const siteSlogan = 'Közhasznú Sport Egyesület';
+export const facebookUrl = 'https://www.facebook.com/kisgalya.kse/';
